6bbd1ec4b31afa036fa0dbf2d86aefdad2adc4de
Pete Bacon Darwin <pete@bacondarwin.com>
2010-02-21 22:10:57 +0000

    Fixes to ensure that folders are recreated if they were deleted unexpectedly and to ensure that they are moved correctly when pages move

--- code/AssociatedFolderDecorator.php
+++ code/AssociatedFolderDecorator.php
@@ -7,140 +7,135 @@
  * DataObject::add_extension('MyDataObject', 'AssociatedFolderDecorator');
  *
  */
-class AssociatedFolderDecorator extends DataObjectDecorator {
-	function extraDBFields() {
-		return array(
+class AssociatedFolderDecorator extends SiteTreeDecorator {
+    function extraDBFields() {
+        return array(
 			'has_one' => array(
 				'AssociatedFolder' => 'Folder'
 				)
 				);
-	}
+    }
 
-	/**
-	 * The folder name to use if the decorated object's parent has no associated folder of its own.
-	 *
-	 * @var string
-	 */
-	protected static $defaultRootFolderName = "associated-folders";
-	/**
-	 * Set the folder name to use if the decorated object's parent has no associated folder of its own.
-	 *
-	 * @param string $folderName
-	 */
-	static function setDefaultRootFolderName($folderName) {
-		AssociatedFolderDecorator::$defaultRootFolderName = $folderName;
-	}
-	/**
-	 * Get the folder name to use if the decorated object's parent has no associated folder of its own.
-	 *
-	 * @return string
-	 */
-	static function getDefaultRootFolderName() {
-		return AssociatedFolderDecorator::$defaultRootFolderName;
-	}
-	
-	/**
-	 * Do stuff before the decorated object is written.
-	 * In this case create the associated folder if it does not already exist.
-	 *
-	 */
-	function onBeforeWrite() {
-		parent::onBeforeWrite();
-		if( ! $this->owner->AssociatedFolderID ) {
-			$this->createAssociatedFolder();
-		}
-	}
-	
-	/**
-	 * Create the associated folder
-	 *
-	 * @return Folder
-	 */
-	function createAssociatedFolder() {
-		$parent = $this->owner->Parent;
-		if ( $parent && $parent->hasExtension($this->class) ) {
-			$parentFolder = $parent->AssociatedFolder();
-			$parentFolderName = str_replace('assets/','',$parentFolder->FileName);
-		} else {
-			$parentFolderName = $this->stat('defaultRootFolderName');
-		}
-		$associatedFolder = Folder::findOrMake($parentFolderName . '/' . $this->owner->URLSegment);
-		$this->owner->AssociatedFolderID = $associatedFolder->ID;
-		return $associatedFolder;
-	}
+    /**
+     * The folder name to use if the decorated object's parent has no associated folder of its own.
+     *
+     * @var string
+     */
+    protected static $defaultRootFolderName = "associated-folders";
+    /**
+     * Set the folder name to use if the decorated object's parent has no associated folder of its own.
+     *
+     * @param string $folderName
+     */
+    static function setDefaultRootFolderName($folderName) {
+        AssociatedFolderDecorator::$defaultRootFolderName = $folderName;
+    }
+    /**
+     * Get the folder name to use if the decorated object's parent has no associated folder of its own.
+     *
+     * @return string
+     */
+    static function getDefaultRootFolderName() {
+        return AssociatedFolderDecorator::$defaultRootFolderName;
+    }
 
-	/**
-	 * Do stuff after the decorated object is written.
-	 * In this case update the associated folder to match the title of the decorated object.
-	 *
-	 */
-	function onAfterWrite() {
-		if( $this->owner->ID ) {
-			$this->updateAssociatedFolder();
-		}
-		parent::onAfterWrite();
-	}
+    /**
+     * Do stuff before the decorated object is written.
+     * In this case create the associated folder if it does not already exist.
+     *
+     */
+    function onBeforeWrite() {
+        parent::onBeforeWrite();
 
-	/**
-	 * Update the folder that holds the photos to have the same file name as this page's title.
-	 *
-	 */
-	function updateAssociatedFolder() {
-		$changedFields = $this->owner->getChangedFields();
-		$parentChanged = array_key_exists('ParentID', $changedFields );
-		$folder = $this->owner->AssociatedFolder();
-		if ( $folder ) {
-			$folder->Title = $this->owner->Title;
-			$folder->Name = $this->owner->URLSegment;
-			if ( $parentChanged ) {
-				// The parent has changed move the folder if possible
-				$parent = DataObject::get_by_id('Page', $changedFields['ParentID']['after']);
-				if ( $parent &&  $parent->hasExtension($this->class) ) {
-					// Move the folder to the new parent
-					$folder->ParentId = $parent->AssociatedFolder()->ID;
-				} else {
-					// Move the folder below the default root as its owner's parent is not folder associated
-					$root = Folder::findOrMake($this->stat('defaultRootFolderName'));
-					$folder->ParentId = $root->ID;
-				}
-			}
-			$folder->write();
-		} else {
-			trigger_error("Associated folder does not exist", E_USER_ERROR);
-		}
-	}
+        $folder = null;
+        
+        // Check whether the folder id is 0 or the Folder object does not exist
+        if ( $this->owner->AssociatedFolderID != 0 ) {
+            $folder = DataObject::get_by_id('Folder', $this->owner->AssociatedFolderID);
+        }
 
-	/**
-	 * Do stuff before the decorated object is deleted.
-	 * In this case unlink (delete) the associated folder.
-	 *
-	 */
-	function onBeforeDelete() {
-		parent::onBeforeDelete();
-		$this->deleteFolderAssociatedChildPages();
-		$folder = $this->owner->AssociatedFolder();
-		if ( $folder ) {
-			$folder->delete();
-		} else {
-			trigger_error("Associated folder does not exist", E_USER_ERROR);
-		}
-	}
+        if( !$folder ) {
+            // A valid folder does not exist
+            $this->createAssociatedFolder();
+        } else {
+            if ( !file_exists($folder->getFullPath()) ) {
+                // The folder object may exist but the folder on the file system does not.
+                mkdir($folder->getFullPath(),Filesystem::$folder_create_mask);
+            }
+        }
+    }
+    
+    /**
+     * Create the associated folder
+     *
+     * @return Folder
+     */
+    function createAssociatedFolder() {
+        $parent = $this->owner->Parent;
+        if ( $parent && $parent->hasExtension($this->class) ) {
+            $parentFolder = $parent->AssociatedFolder();
+            $parentFolderName = str_replace('assets/','',$parentFolder->FileName);
+        } else {
+            $parentFolderName = $this->stat('defaultRootFolderName');
+        }
+        $associatedFolder = Folder::findOrMake($parentFolderName . '/' . $this->owner->URLSegment);
+        $this->owner->AssociatedFolderID = $associatedFolder->ID;
+        return $associatedFolder;
+    }
 
-	/**
-	 * Silverstripe does not delete child objects by default.
-	 * This seems a bit weird behaviour and since deleting this object's associated folder will
-	 * delete all sub-folders we ought to delete any child pages that are associated with
-	 * folders to stop them from becoming bad.
-	 *
-	 */
-	function deleteFolderAssociatedChildPages() {
-		$childPages = DataObject::get('Page', 'ParentId = '.$this->owner->ID);
-		if ( $childPages ) {
-			foreach($childPages as $childPage) {
-				if ( $childPage->hasExtension($this->class) ) {
-					$childPage->delete();
-				}
-			}
-		}
-	}
+    /**
+     * Do stuff after the decorated object is published.
+     * In this case update the associated folder to match the title of the decorated object.
+     * This way the filename and position in the folder tree always matches the published version.
+     *
+     */
+    function onAfterPublish($original) {
+        parent::onAfterPublish($original);
+        if( $this->owner->ID ) {
+            $this->updateAssociatedFolder($original);
+        }
+    }
+
+    /**
+     * Update the folder that holds the photos to have the same file name as this page's title.
+     *
+     */
+    function updateAssociatedFolder($original) {        
+        $folder = $this->owner->AssociatedFolder();
+        if ( $this->owner->AssociatedFolderID && $folder ) {
+            $folder->Title = $this->owner->Title;
+            $folder->Name = $this->owner->URLSegment;
+            if ( $this->owner->parentID != $original->ParentID ) {
+                // The parent has changed move the folder if possible
+                $parent = $this->owner->Parent();
+                if ( $parent &&  $parent->hasExtension($this->class) ) {
+                    // Move the folder to the new parent
+                    $folder->ParentId = $parent->AssociatedFolderID;
+                } else {
+                    // Move the folder below the default root as its owner's parent is not folder associated
+                    $root = Folder::findOrMake($this->stat('defaultRootFolderName'));
+                    $folder->ParentId = $root->ID;
+                }
+            }
+            $folder->write();
+        } else {
+            trigger_error("Associated folder does not exist", E_USER_ERROR);
+        }
+    }
+
+    /**
+     * Do stuff before the decorated object is deleted.
+     * In this case rename the associated folder.
+     *
+     */
+    function onBeforeUnpublish() {
+        parent::onBeforeUnpublish();
+        $folder = $this->owner->AssociatedFolder();
+        if ( $folder ) {
+            $folder->Name = $this->owner->URLSegment . '__deleted';
+            $folder->write();
+        } else {
+            trigger_error("Associated folder does not exist", E_USER_ERROR);
+        }
+    }
 }
